f8fd978c199cf5036a0ae7a81a9329412c140c9e
marcos daniel santana <Marcos.Daniel1990@hotmail.com>
2022-08-13 00:28:28 -0300

    coletando tickets

--- predição do mini índice/01-Metatrade 5/01-coletando ticket dos ativos/main.py
+++ predição do mini índice/01-Metatrade 5/01-coletando ticket dos ativos/main.py
@@ -2,4 +2,21 @@ import MetaTrader5 as mt5       #importa a biblioteca
 
 mt5.initialize()        #inicia o metatrader 5
 
-tickets  = mt5.symbols_total()      # recebe os tickets dos ativos
+ticketsTotal  = mt5.symbols_total()      # recebe a quantidade de tickets dos ativos
+
+if ticketsTotal > 0:
+    print("Quantidade total de tickets de ativos: ", ticketsTotal)
+else:
+    print("Síbolos não encontrados!")
+
+ticket = mt5.symbols_get()      #recebe o ticket dos ativos
+cont = 0
+for i in ticket:
+    if cont < 100:
+        print("{}: {}".format(cont, i.name))    # imprime o ticket dos ativos
+
+    else:
+        break
+    cont += 1
+
+mt5.shutdown()      # encerra o metatrader
