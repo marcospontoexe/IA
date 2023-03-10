7d04f2faa84e7626ca601f5ff52f70e7e478b38a
marcos <marcos.daniel1990@hotmail.com>
2023-03-10 13:14:02 -0300

    Update TratamentoDados.py

--- predição do mini índice/02-Tratamento de dados/TratamentoDados.py
+++ predição do mini índice/02-Tratamento de dados/TratamentoDados.py
@@ -3,6 +3,6 @@ import pandas as pd
 selecionarCol = ["<DATE>", "<TIME>", "<OPEN>", "<HIGH>", "<LOW>", "<CLOSE>", "<VOL>"] # seleciona quais colunas o dataframe deve possuir
 dfBruto = pd.read_csv("WIN$_H1.csv", sep="\t", usecols=selecionarCol)
 
-dfTratado = pd.DataFrame()
-
-print(df)
+dfTratado = pd.DataFrame({"Tamanho", "Cor", "Variação de preço"})
+print(dfTratado)
+print(type(dfTratado))
