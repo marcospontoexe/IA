833d101332d949e3b217f2711975ba35df76685d
marcos <marcos.daniel1990@hotmail.com>
2023-03-10 13:11:02 -0300

    tratamento de dados

---
+++ predição do mini índice/02-Tratamento de dados/TratamentoDados.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+selecionarCol = ["<DATE>", "<TIME>", "<OPEN>", "<HIGH>", "<LOW>", "<CLOSE>", "<VOL>"] # seleciona quais colunas o dataframe deve possuir
+dfBruto = pd.read_csv("WIN$_H1.csv", sep="\t", usecols=selecionarCol)
+
+dfTratado = pd.DataFrame()
+
+print(df)
