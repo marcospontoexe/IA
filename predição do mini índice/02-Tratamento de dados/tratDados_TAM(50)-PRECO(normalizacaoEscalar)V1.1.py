27b1496ab9aeeb1f6fb7d558041c42c02afa01b8
marcos daniel santana <Marcos.Daniel1990@hotmail.com>
2023-03-28 00:31:52 -0300

    .

---
+++ predição do mini índice/02-Tratamento de dados/tratDados_TAM(50)-PRECO(normalizacaoEscalar)V1.1.py
@@ -0,0 +1,242 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+
+selecionarCol = ["<DATE>", "<TIME>", "<OPEN>", "<HIGH>", "<LOW>", "<CLOSE>", "<VOL>"] # seleciona quais colunas o dataframe deve possuir
+dfBruto = pd.read_csv("WIN$_H1.csv", sep="\t", usecols=selecionarCol)
+filtro = dfBruto["<TIME>"] != "18:00:00"        # filtra apenas as velas das 9h às 17h
+dfTratado = dfBruto[filtro].loc[:, ["<DATE>", "<TIME>"]].copy()     # copia para novo dataframe apenas as colunas "<DATE>" e "<TIME>", mantendo os mesos índices
+
+
+
+# -----verificando se o dataframe contem todos as velas 9h-17h) do dia-------------
+condicao = dfTratado['<DATE>'].value_counts() < 9                       # TRUE para dias que tem menos de 9 velas (das 9h às 17h)
+datas = ((dfTratado['<DATE>'].value_counts()[condicao]).index).values   # datas dos dias incompletos
+print('As seguintes datas estão incompletas e foram apagadas:')
+for i in range(0, len(datas)):                                          # apagando o dia incompleto
+    print(f"{datas[i]}")                                                # mostra a data excluida do dftratado
+    datasIndice = ((dfTratado['<DATE>']) == datas[i])                   # recebe um dataframe com 'True' nas linhas do dia comparado com a data
+    lista = dfTratado[datasIndice]                         # recebe um dataframe com as linhas do dia comparado
+    listaIndice = lista.index  # recebe uma lista com os índices do dataframe 'lista'
+    dfTratado.drop(listaIndice, inplace=True)        # apaga as linhas dos dias incompletos
+# ---------------------------------------------------------
+
+
+#-----Criando novas colunas-------------------------
+dfTratado['<TAMANHO>'] = np.nan                                 # tam (tamanho da vela)
+dfTratado['<TAMANHO NORMALIZADO>'] = np.nan                     # tamNor (normalização do tamanho da vela)
+dfTratado['<VARIAÇÃO DO PREÇO>'] = np.nan                       # var (variação de preço)
+dfTratado['<VARIAÇÃO DO PREÇO NORMALIZADO>'] = np.nan           # varNor (normalização da variação de preço)
+dfTratado['<PREÇO>'] = np.nan                                   # preco (valor de abertura normalizado)
+dfTratado['<VOLUME NORMALIZADO>'] = np.nan                      # volNor (volume normalizado)
+dfTratado['<TAMANHO MÉDIO NORMALIZADO DAS VELAS>'] = np.nan     # tamMed (é o tamanho médio entre as velas do dia)
+dfTratado['<VOLUME MÉDIO NORMALIZADO DAS VELAS>'] = np.nan      # volMed (é o volume médio normalizado entre as velas do dia)
+dfTratado['<OPERAÇÃO>'] = np.nan                                # Compra, Venda ou lateral
+#-----------------------------------------------
+
+
+#----------------Tratando o dataframe------------------
+dfTratado['<TAMANHO>'] = dfBruto['<CLOSE>'] - dfBruto['<OPEN>']       # tam (tamanho da vela)
+dfTratado['<VARIAÇÃO DO PREÇO>'] = dfBruto['<HIGH>'] - dfBruto['<LOW>']  # var (variação de preço)
+somaTamanho = 0
+for indice, coluna in dfTratado.iterrows():
+    if (coluna["<TIME>"] == "09:00:00"):    # para as velas das 9h às 16h
+        valorMax = float(dfBruto.loc[indice:indice + 7, ["<HIGH>"]].max().values)  # maior preço de negociação no dia (das 9h às 16h)
+        valorMin = float(dfBruto.loc[indice:indice + 7, ["<LOW>"]].min().values)  # menor preço de negociação no dia (das 9h às 16h)
+        volMax = float(dfBruto.loc[indice:indice+7, ["<VOL>"]].max().values)# (maior volume encontrado no dia, das 9h às 16h)
+        volMin = float(dfBruto.loc[indice:indice+7, ["<VOL>"]].min().values)  # (menor volume encontrado no dia, das 9h às 16h)
+        varMax = int(dfTratado.loc[indice:indice+7,["<VARIAÇÃO DO PREÇO>"]].max().values)  # maior variação de preço do dia das 9h às 16h
+        varMin = int(dfTratado.loc[indice:indice+7, ["<VARIAÇÃO DO PREÇO>"]].min().values)  # menor variação de preço do dia das 9h às 16h
+        tamMax = int(abs(dfTratado.loc[indice:indice+7, ["<TAMANHO>"]]).max().values)  # maior tamanho de vela do dia das 9h às 16h
+        tamMin = int(abs(dfTratado.loc[indice:indice+7,["<TAMANHO>"]]).min().values)  # menor tamanho de vela do dia das 9h às 16h
+
+        dfTratado.loc[indice:indice+8, ["<TAMANHO NORMALIZADO>"]] = ((dfTratado.loc[indice:indice+8, ["<TAMANHO>"]]).values)  / tamMax # tamNor (normalização do tamanho da vela)
+        dfTratado.loc[indice:indice+8, ["<VARIAÇÃO DO PREÇO NORMALIZADO>"]] = ((((dfTratado.loc[indice:indice+8, ["<VARIAÇÃO DO PREÇO>"]]).values))-varMin) / (varMax - varMin)  # varNor (normalização da variação de preço)
+        dfTratado.loc[indice:indice+8, ['<PREÇO>']] = (((dfBruto.loc[indice:indice+8, ["<OPEN>"]]).values) - valorMin) / (valorMax - valorMin) # preco (valor de abertura normalizado)
+        dfTratado.loc[indice:indice+8, ['<VOLUME NORMALIZADO>']] = ((((dfBruto.loc[indice:indice+8, ["<VOL>"]]).values)) - volMin)/ (volMax - volMin) # volNor (volume normalizado)
+
+
+
+        somaTamanho = abs(dfTratado.loc[indice:indice+7, ["<TAMANHO NORMALIZADO>"]].values).sum()   # soma o valor absoluto do tamanho normalizado das velas das 9h às 16h
+        somaVolume = abs(dfTratado.loc[indice:indice+7, ["<VOLUME NORMALIZADO>"]].values).sum()     # soma o valor absoluto do volume normalizado das velas das 9h às 16h
+        dfTratado.loc[[indice+8], ['<TAMANHO MÉDIO NORMALIZADO DAS VELAS>']] = somaTamanho / 8      # tamMed (é o tamanho médio entre as velas do dia)
+        dfTratado.loc[[indice+8], ['<VOLUME MÉDIO NORMALIZADO DAS VELAS>']] = somaVolume / 8        # volMed (é o volume médio normalizado entre as velas do dia)
+
+
+        #VARIÁVEIS TRATADAS PARA TREINO (COMPRA)
+        if( ((int(dfTratado.loc[[indice+8], ["<TAMANHO>"]].values)) > 0) and ((abs(float(dfTratado.loc[[indice+8],["<TAMANHO NORMALIZADO>"]].values))) >= (float(dfTratado.loc[[indice+8],['<TAMANHO MÉDIO NORMALIZADO DAS VELAS>']].values)*1)) ):
+            dfTratado.loc[[indice+8], ['<OPERAÇÃO>']] = 'COMPRA'
+
+        # VARIÁVEIS TRATADAS PARA TREINO (venda)
+        elif( ((int(dfTratado.loc[[indice+8], ["<TAMANHO>"]].values)) < 0) and ((abs(float(dfTratado.loc[[indice+8],["<TAMANHO NORMALIZADO>"]].values))) >= (float(dfTratado.loc[[indice+8],['<TAMANHO MÉDIO NORMALIZADO DAS VELAS>']].values)*1)) ):
+            dfTratado.loc[[indice+8], ['<OPERAÇÃO>']] = 'VENDA'
+
+        # VARIÁVEIS TRATADAS PARA TREINO (LATERAL)
+        else:
+            dfTratado.loc[[indice+8],['<OPERAÇÃO>']] = 'LATERAL'
+#----------------------------------------------------
+
+dfTratado.to_excel("dfTratado.xlsx")      # salva como csv, sem os índices         #############################################
+
+#------mostra quantas saidas foram geradas------
+print(f"Quantidade de rótulos antes da equalização:")
+filtroCompra = dfTratado['<OPERAÇÃO>'] == 'COMPRA'   # recebe uma série contendo, "True" quando os valores da coluna "<OPERAÇÃO>" é "COMPRA", e "False" caso contrário
+compra = sum(filtroCompra * 1)
+print(f"QUANTIDADE DE COMPRAS: {compra}")          # mostra quantas linhas que contém o valor "John"
+filtroVenda = dfTratado['<OPERAÇÃO>'] == 'VENDA'   # recebe uma série contendo, "True" quando os valores da coluna "<OPERAÇÃO>" é "COMPRA", e "False" caso contrário
+venda = sum(filtroVenda * 1)
+print(f"QUANTIDADE DE VENDA: {venda}")          # mostra quantas linhas que contém o valor "John"
+filtroLateral = dfTratado['<OPERAÇÃO>'] == 'LATERAL'   # recebe uma série contendo, "True" quando os valores da coluna "<OPERAÇÃO>" é "COMPRA", e "False" caso contrário
+lateral = sum(filtroLateral * 1)
+print(f"QUANTIDADE DE LATERAL: {lateral}")          # mostra quantas linhas que contém o valor "John"
+menor = min(compra, venda, lateral)
+print(f"menor quantidade: {menor}")
+#---------------------------------------------
+
+
+
+#----Mantem proporção de valores entre as classes------
+if(compra > menor):     # caso a quantidadede 'COMPRA' não seja o menor valor
+    datasCompra = dfTratado[filtroCompra]['<DATE>'].values      # array contendo as datas de '<OPERAÇÃO>' == 'COMPRA'
+    difer = compra-menor
+    for i in range(0,difer):
+        indiceCompras = ((dfTratado[dfTratado['<DATE>'] == datasCompra[i]]).index).values   # array com os índices de cada data
+        dfTratado.drop(indiceCompras, inplace=True)             # apaga os índices das datas 'datasCompra'
+
+
+if(venda > menor):     # caso a quantidadede 'VENDE' não seja o menor valor
+    datasVenda = dfTratado[filtroVenda]['<DATE>'].values      # array contendo as datas de '<OPERAÇÃO>' == 'VENDA'
+    difer = venda-menor
+    for i in range(0,difer):
+        indiceVendas = ((dfTratado[dfTratado['<DATE>'] == datasVenda[i]]).index).values
+        dfTratado.drop(indiceVendas, inplace=True)
+
+
+if(lateral > menor):         # caso a quantidadede 'LATERAL' não seja o menor valor
+    datasLateral = dfTratado[filtroLateral]['<DATE>'].values  # array contendo as datas de '<OPERAÇÃO>' == 'LATERAL'
+    difer = lateral - menor
+    for i in range(0, difer):
+        indiceLateral = ((dfTratado[dfTratado['<DATE>'] == datasLateral[i]]).index).values
+        dfTratado.drop(indiceLateral, inplace=True)
+#------------------------------------------------------
+
+#------mostra quantas saidas foram geradas------
+print(f"Quantidade de rótulos após a equalização:")
+filtroCompra = dfTratado['<OPERAÇÃO>'] == 'COMPRA'   # recebe uma série contendo, "True" quando os valores da coluna "<OPERAÇÃO>" é "COMPRA", e "False" caso contrário
+compra = sum(filtroCompra * 1)
+print(f"QUANTIDADE DE COMPRAS: {compra}")          # mostra quantas linhas que contém o valor "John"
+filtroVenda = dfTratado['<OPERAÇÃO>'] == 'VENDA'   # recebe uma série contendo, "True" quando os valores da coluna "<OPERAÇÃO>" é "COMPRA", e "False" caso contrário
+venda = sum(filtroVenda * 1)
+print(f"QUANTIDADE DE VENDA: {venda}")          # mostra quantas linhas que contém o valor "John"
+filtroLateral = dfTratado['<OPERAÇÃO>'] == 'LATERAL'   # recebe uma série contendo, "True" quando os valores da coluna "<OPERAÇÃO>" é "COMPRA", e "False" caso contrário
+lateral = sum(filtroLateral * 1)
+print(f"QUANTIDADE DE LATERAL: {lateral}")          # mostra quantas linhas que contém o valor "John"
+menor = min(compra, venda, lateral)
+#---------------------------------------------
+
+#------reorganização do dataframe-------------
+'''dfReordenado = pd.DataFrame({          # cria um dataframe com apenas uma linha
+    '<DATE>':[1],'<TIME>':[1],'<TAMANHO>':[1],'<TAMANHO NORMALIZADO>':[1],'<VARIAÇÃO DO PREÇO>':[1],
+    '<VARIAÇÃO DO PREÇO NORMALIZADO>':[1],'<PREÇO>':[1],'<VOLUME NORMALIZADO>':[1],
+    '<TAMANHO MÉDIO NORMALIZADO DAS VELAS>':[1],'<VOLUME MÉDIO NORMALIZADO DAS VELAS>':[1],
+    '<OPERAÇÃO>':[1]
+})'''
+dfReordenado = dfTratado.copy()
+tam = dfTratado.shape[0]    # quantidade de linhas do dftratado
+filtroCompra1 = dfTratado['<OPERAÇÃO>'] == 'COMPRA'   # recebe uma série contendo, "True" quando os valores da coluna "<OPERAÇÃO>" é "COMPRA", e "False" caso contrário
+filtroVenda1 = dfTratado['<OPERAÇÃO>'] == 'VENDA'
+filtroLateral1 = dfTratado['<OPERAÇÃO>'] == 'LATERAL'
+datasCompra1 = dfTratado[filtroCompra1]['<DATE>'].values      # array contendo as datas de '<OPERAÇÃO>' == 'COMPRA'
+datasVenda1 = dfTratado[filtroVenda1]['<DATE>'].values      # array contendo as datas de '<OPERAÇÃO>' == 'COMPRA'
+datasLateral1 = dfTratado[filtroLateral1]['<DATE>'].values      # array contendo as datas de '<OPERAÇÃO>' == 'COMPRA'
+indiceDfReordenado = dfReordenado.index.values
+cont = 0
+for i in range(0, len(datasCompra1) ):
+    indiceCompras1 = ((dfTratado[dfTratado['<DATE>'] == datasCompra1[i]]).index).values  # array com os índices de cada data
+    indiceVendas1 = ((dfTratado[dfTratado['<DATE>'] == datasVenda1[i]]).index).values  # array com os índices de cada data
+    indiceLateral1 = ((dfTratado[dfTratado['<DATE>'] == datasLateral1[i]]).index).values  # array com os índices de cada data
+    for j in range(0, len(indiceCompras1)):
+        dfReordenado.loc[[indiceDfReordenado[cont]]] = dfTratado.loc[[indiceCompras1[j]]].values
+        cont = cont+1
+    for j in range(0, len(indiceVendas1)):
+        dfReordenado.loc[[indiceDfReordenado[cont]]] = dfTratado.loc[[indiceVendas1[j]]].values
+        cont = cont+1
+    for j in range(0, len(indiceLateral1)):
+        dfReordenado.loc[[indiceDfReordenado[cont]]] = dfTratado.loc[[indiceLateral1[j]]].values
+        cont = cont+1
+#---------------------------------------------
+
+#-----Novo dataframe apenas com as linha últeis---------------------
+colunasDf = ["<TIME>", "<PREÇO>", "<TAMANHO NORMALIZADO>", "<VARIAÇÃO DO PREÇO NORMALIZADO>", "<VOLUME NORMALIZADO>", "<OPERAÇÃO>"] # seleciona quais colunas o dataframe deve possuir
+dfLimpo = dfReordenado[colunasDf].copy()
+
+#--------------------------------------------------------------------
+
+'''#--------visualizando o dataframe----------
+plt.boxplot(dfLimpo["<TAMANHO NORMALIZADO>"])
+plt.title("TAMANHO DA VELA")
+plt.xlabel("TAMANHO NORMALIZADO")
+plt.ylabel("VALOR")
+plt.show()
+#-----------------------------------------
+'''
+#-----verificando a correlação--------
+cor = dfLimpo.loc[:,["<PREÇO>", "<TAMANHO NORMALIZADO>", "<VARIAÇÃO DO PREÇO NORMALIZADO>", "<VOLUME NORMALIZADO>"]].corr()
+#cor.style.background_gradient(cmap='coolwarm')
+print(cor)
+#--------------------------------------
+
+#-----Gerando o dataframe para a RNA---------
+dfRna = pd.DataFrame({          # cria um dataframe com apenas uma linha
+    'X1':[1],'X2':[1],'X3':[1],'X4':[1],'X5':[1],'X6':[1],'X7':[1],'X8':[1],'X9':[1],'X10':[1],
+    'X11':[1],'X12':[1],'X13':[1],'X14':[1],'X15':[1],'X16':[1],'X17':[1],'X18':[1],'X19':[1],'X20':[1],
+    'X21':[1],'X22':[1],'X23':[1],'X24':[1],'X25':[1],'X26':[1],'X27':[1],'X28':[1],'X29':[1],'X30':[1],
+    'X31':[1],'X32':[1], 'Y1':[1],'Y2':[1],'Y3':[1]
+})
+contColuna = 0  # iteração para incluir colunas no dataframe 'dfRNA'
+contLinha = 0   # iteração para incluir linhas no dataframe 'dfRNA'
+for linha, dado in dfLimpo.iterrows():
+    listaLinha = (dado[1:].values)  # recebe uma lista em float com os valores da linha atual do dataframe (sem a coluna '<TIME>')
+    if (dado["<TIME>"] != "17:00:00"):  # para horário das 9h às 16h a coluna '<OPERAÇÃO>' não tem nenhum valor
+        for i in range(0,4):
+            contColuna = contColuna + 1
+            dfRna.loc[contLinha,[f"X{contColuna}"]] = listaLinha[i]
+
+    else:           # para horário das 17h somente a coluna '<OPERAÇÃO>' deve ser incluida no datafrme 'dfRna'
+
+        if(listaLinha[4] == "COMPRA"):
+            dfRna.loc[[contLinha], ["Y1"]] = 1
+            dfRna.loc[[contLinha], ["Y2"]] = 0
+            dfRna.loc[[contLinha], ["Y3"]] = 0
+        elif(listaLinha[4] == "LATERAL"):
+            dfRna.loc[[contLinha], ["Y1"]] = 0
+            dfRna.loc[[contLinha], ["Y2"]] = 1
+            dfRna.loc[[contLinha], ["Y3"]] = 0
+        elif(listaLinha[4] == "VENDA"):
+            dfRna.loc[[contLinha], ["Y1"]] = 0
+            dfRna.loc[[contLinha], ["Y2"]] = 0
+            dfRna.loc[[contLinha], ["Y3"]] = 1
+        else:
+            print(f'ERRO: {listaLinha[4]}')
+
+        contColuna = 0
+        contLinha = contLinha + 1
+
+#print((dfRna))
+dfBruto.to_excel("dfBruto.xlsx")                                #############################################################
+dfLimpo.to_excel("dfLimpo.xlsx")      # salva como csv, sem os índices      ###########################################
+cor.to_excel("correlação.xlsx")      # salva como csv, sem os índices      ##################################################
+dfRna.to_excel("dfRna.xlsx", index=False)      # salva sem os índices
+dfRna.to_csv("dfRna.csv", index=False)      # salva sem os índices
+dfReordenado.to_excel("dfReordenado.xlsx")      # salva sem os índices
+
+
+
+
+
+
+
+
--- predição do mini índice/03- RNA/01-treinamento da rna/RecurrentNetwork_V1.1.py
+++ predição do mini índice/03- RNA/01-treinamento da rna/RecurrentNetwork_V1.1.py
@@ -1,19 +1,21 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-#import pybrain   # pip install https://github.com/pybrain/pybrain/archive/0.3.3.zip
+import pybrain   # pip install https://github.com/pybrain/pybrain/archive/0.3.3.zip
 
 import matplotlib.pyplot as plt
+
+
 from pybrain.tools.shortcuts import buildNetwork	# usado para construir a estrutura de uma rna
+from pybrain.structure import RecurrentNetwork, IdentityConnection
 from pybrain.datasets import SupervisedDataSet		# método de aprendizagem (supervisionado)
+from pybrain.structure import FullConnection		# para fazer a conexão entre as camadas
+from pybrain.structure.connections.connection import Connection					# para criar conexões parciais
 from pybrain.supervised.trainers import BackpropTrainer	#método de treiamento supervisionado
 #Sigmoid activation functions are used when the output of the neural network is continuous. Softmax activation functions are used when the output of the neural network is categorical.
 #https://towardsdatascience.com/activation-functions-neural-networks-1cbd9f8d91d6
-from pybrain.structure.modules import SoftmaxLayer, SigmoidLayer 	# funções de ativação
-from pybrain.structure.modules import LSTMLayer
-from pybrain.structure.modules import TanhLayer
-from pybrain.structure.modules import LinearLayer
-from pybrain.structure.modules import BiasUnit		# bias
+from pybrain.structure import SoftmaxLayer, LSTMLayer, SigmoidLayer, TanhLayer, LinearLayer		# funções de ativação
+from pybrain.structure import BiasUnit		# bias
 from pybrain.tools.customxml import NetworkWriter	# para salvar em xml
 
 seed = 176
@@ -29,13 +31,11 @@ nOutputs = 3
 df = pd.read_csv('dfRna.csv', header=None)
 tamanhoTreino = int((df.shape[0])*0.75)		# define um tamanho de 75% do banco de dados para usar como treinamento. (25% como daddos para validação)
 
-
 dfTreino = df.loc[1:tamanhoTreino].copy()		# dataframe para treinar a rna
 dfValidacao	= df.loc[(tamanhoTreino+1):].copy()	# dataframe para validação da rna
 X_train = dfTreino.iloc[:, 0:nInputs].values    # array com valores de entrada da rna
 y_train = dfTreino.iloc[:, nInputs:(nInputs+nOutputs)].values   # array com valores para as saidas da rna
 
-#df.to_excel("df.xlsx")
 #dfTreino.to_excel("dfTreino.xlsx")      # salva como csv, sem os índices      ###########################################
 #dfValidacao.to_excel("dfValidacao.xlsx")      # salva como csv, sem os índices      ###########################################
 #print(f"tamanho: {tamanhoTreino}")
@@ -44,24 +44,54 @@ y_train = dfTreino.iloc[:, nInputs:(nInputs+nOutputs)].values   # array com valo
 
 # Construcao da rede neural
 #rede = buildNetwork(nInputs, hidden_layers, nOutputs, bias=True, hiddenclass=TanhLayer ou LSTMLayer, outclass=SoftmaxLayer)
-rede = buildNetwork(nInputs, 18, 11, 6, nOutputs,hiddenclass=TanhLayer, outclass=SoftmaxLayer, bias=True)
-'''When building networks with the buildNetwork shortcut, the parts are named
-automatically:
->>> net[’in’]
-<LinearLayer ’in’>
->>> net[’hidden0’]
-<SigmoidLayer ’hidden0’>
->>> net[’out’]
-<LinearLayer ’out’>
-OBS: o atalho buildNetwork permite topologia apenas feedforward
-'''
+rede = RecurrentNetwork()			# cria uma rna recorrente
+	#(nInputs,16, 10, 8,5, nOutputs, bias=True, outclass=SoftmaxLayer, outputbias=True)
+#-----criando as camadas com seus neurônios------------------------
+input_layer = LinearLayer(nInputs)  	# cria uma camada de entrada
+hidden0 = SigmoidLayer(20)			# cria uma camada oculta
+hidden1 = SigmoidLayer(5)			# cria uma camada oculta
+output_layer = LinearLayer(nOutputs)  #cria camada de saida
+biasHidden0 = BiasUnit()			# cria um bias para a camada oculta 0
+biasHidden1 = BiasUnit()			# cria um bias para a camada oculta
+biasOut = BiasUnit()			# cria um bias para a camada de saida
+
+#------------adicionando as camadas na rna------------
+rede.addInputModule(input_layer)		# adiciona uma camada de entrada
+rede.addModule(hidden0)				# adiciona camada oculta
+rede.addModule(hidden1)				# adiciona camada oculta
+rede.addOutputModule(output_layer)	# adiciona camada de saida
+rede.addModule(biasHidden0)				# adiciona um bias a camada oulta
+rede.addModule(biasHidden1)				# adiciona um bias a camada oulta
+rede.addModule(biasOut)				# adiciona um bias a camada de saida
+#---------------------------------------------------------------
+
+#-----------------configurando as conexão entre as camadas------------------
+in_to_hidden0 = FullConnection(input_layer, hidden0)		# cria um modo de conexão entre camada de entrada e oculta0
+hidden0_to_hidden1 = FullConnection(hidden0, hidden1)		# cria um modo de conexão entre as camadas ocultas
+hidden1_to_output = FullConnection(hidden1, output_layer)		# cria um modo de conexão conexão entre a camada oculta e camada de saida
+biasHidden0_to_hidden0 = FullConnection(biasHidden0, hidden0) # cria um modo de conexão entre bias0 e camada oculta 0
+biasHidden1_to_hidden1 = FullConnection(biasHidden1, hidden1) # cria um modo de conexão entre bias1 e camada oculta 1
+biasOut_to_out = FullConnection(biasOut, output_layer) # cria um modo de conexão entre biasout e camada de saida
+hidden0_to_hidden0 = IdentityConnection(hidden0, hidden0)		# cria um modo de conexão entre as camadas ocultas 0 sem alteração do peso sináptico
+hidden1_to_hidden1 = IdentityConnection(hidden1, hidden1)		# cria um modo de conexão entre as camadas ocultas 1 sem alteração do peso sináptico
+
+rede.addConnection(in_to_hidden0)
+rede.addConnection(hidden0_to_hidden1)		# adidiona conexão entre as camadas ocultas
+rede.addConnection(hidden1_to_output)		# adidiona conexão entre a oculta e a saida
+rede.addConnection(biasHidden0_to_hidden0)	# adidiona conexão entre a bias e a camada oculta
+rede.addConnection(biasHidden1_to_hidden1)	#adidiona conexão entre a bias e a camada oculta
+rede.addConnection(biasOut_to_out)	#adidiona conexão entre a bias e a camada de saida
+rede.addRecurrentConnection(hidden0_to_hidden0)	# adicionando as conexões recorrentes
+rede.addRecurrentConnection(hidden1_to_hidden1)	# adicionando as conexões recorrentes
+#---------------------------------------------------------------
+rede.sortModules()				# inicialização da rna
 print(f"RNA: \n{rede}")		# mostra a configuranção da RNA
 #print(f"pesos sinápticos da RNA: \n{rede.params}")		# mostra o valor dos pesos sinápticos da RNA
 #print(f"camada de entrada: {rede['in']}")		# mostra o módo da rede de entrada
 #print(f"camada de oculta0: {rede['hidden0']}")
-
 #print(f"camada de saida: {rede['out']}")
-#print(f"bias: {rede['bias']}")
+#print(f"biasHidden0: {rede['biasHidden0']}")
+#print(f"biasOut: {rede['biasOut']}")
 #print(f"recurrent: {rede['recurrent']}")			# para escolher entre RecurrentNetwork ou FeedForwardNetwork
 
 base = SupervisedDataSet(nInputs, nOutputs)
@@ -72,9 +102,9 @@ for i in range(len(X_train)):
 
 
 # treinamento da rede neural pelo metodo back propagation
-treinamento = BackpropTrainer(rede, dataset = base, learningrate = 0.01, momentum = 0.06, batchlearning=False)
-#treinamento.trainUntilConvergence(maxEpochs=None, verbose=None, continueEpochs=30, validationProportion=0.25)
-epocas = 1000
+treinamento = BackpropTrainer(rede, dataset = base, learningrate = 0.0001, momentum = 0.9, batchlearning=False)
+#treinamento.trainUntilConvergence(maxEpochs=250, verbose=None, continueEpochs=30, validationProportion=0.25)
+epocas = 2000
 learning_rate = np.zeros(epocas)
 for i in range(1, epocas):
     erro = treinamento.train()		# treina a rna pelo método de épocas (usar 'trainer.trainUntilConvergence()' para o método de convergência)
@@ -88,8 +118,6 @@ matrizConfusao = np.zeros((3,3))		# cria um array de duas dimensões 3x3
 for i in range(len(X_train)):
 	y_certo = np.argmax(y_train[i])				# retorna o índice (0, 1, 2, 3...)
 	y_predito = np.argmax(rede.activate(X_train[i]))		# retorna o índice (0, 1, 2, 3...)
-	#print(f"saida da RNA: {rede.activate(X_train[i])}")
-	#print(y_certo)
 	#print(y_predito)
 	#print('---')
 	matrizConfusao[y_certo][y_predito] += 1
